5619ff02f21f11648810316b2117395c76d986b4
DanielPann <152336033+DanielPann@users.noreply.github.com>
2025-10-23 17:46:25 -0500

    Refactor main method for password validation

--- Interpreter/Interpreter/src/Main.java
+++ Interpreter/Interpreter/src/Main.java
@@ -1,27 +1,24 @@
-public static void main(String[] args) {
-        Expression securityRule = buildSecurityRule();
+public class Main {
 
+    public static void main(String[] args) {
+        Expression securityRule = Interpreter.buildSecurityRule();
 
         String[] passwords = {
-            "ValidP4ss!",    
-            "short4",         
-            "minusculas123",  
-            "PasswordLargo"    
+            "ValidP4ss!",
+            "short4",
+            "minusculas123",
+            "PasswordLargo"
         };
 
         for (String pwd : passwords) {
             System.out.printf("%n// VALIDANDO: \"%s\"%n", pwd);
-
-
             boolean isValid = securityRule.interpret(pwd);
 
-
             System.out.println("-------------------------------------");
-            if (isValid) {
-                System.out.println("Contraseña VÁLIDA. Cumple con todas las reglas.");
-            } else {
-                System.out.println("Contraseña INVÁLIDA. Falla alguna regla de seguridad.");
-            }
+            System.out.println(isValid ?
+                "Contraseña VÁLIDA. Cumple con todas las reglas." :
+                "Contraseña INVÁLIDA. Falla alguna regla de seguridad.");
             System.out.println("-------------------------------------");
         }
     }
+}
